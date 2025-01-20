9b6a31a0be6cb004910c16ffd50d2ac93edd46c7
keithkattt <keith110606@gmail.com>
2025-01-20 21:39:41 +0800

    Add modifications in the random number algorithms

--- guessingGame.c
+++ guessingGame.c
@@ -1,14 +1,32 @@
 // A guessing game program
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
 
 //Generates random num from 1 - 100
-int randomNum() 
+int randomNum();
 
 
-int main() {
+int main(){
+    //call randomNum function
+    randomNum();
 
     return 0;
 }
+
+int randomNum(){
+    //Lower Boundary    
+    int lowerBoundary = 1;
+    //Upper Boundary
+    int upperBoundary = 100;
+
+    //random seed and time based on current line
+    srand(time(NULL));
+
+    //loop random generated number up to 5 times
+    for (int i = 1; i <=5; i++){
+        printf("%d\n", rand() % (upperBoundary - lowerBoundary + 1));
+    }
+}
+
 
