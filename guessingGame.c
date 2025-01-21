15dda070cdf1d7d4c5b49d3e75306f21f2b50ffb
keithkattt <k3ith4rwil@gmail.com>
2025-01-21 18:35:30 +0800

    Add guessing game header with delay effect

--- guessingGame.c
+++ guessingGame.c
@@ -1,18 +1,20 @@
 // A guessing game program
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <time.h>
 #include <conio.h>
+#include <windows.h>
 
 int randomNum();//Generates random num from 1 - 100, parameter for how many times you use it 
 int userInput(); //Prompts the user and returns the value
-int asciiArt(); //Prints ASCII art
+void guessingGameHeader(); //Prints ASCII art
 
 int main(){
 
+    guessingGameHeader();
     printf("Welcome to the guessing game!\n");
     printf("I have a number between 1 and 100, can you guess it?\n");
+   
 
     int userGuess = userInput();
     int computerNum = randomNum(0);
@@ -43,15 +45,26 @@ int userInput(){
     return numInput;
 }
 
-int asciiArt(){
-    FILE *file = fopen("asciiArt.txt", "w");
-    if (file == NULL) {
-        perror ("Error opening file");
-        return 1;
-    }
-
-
-    const char *art[] = {
-        
-    }
+void guessingGameHeader(){
+    printf(" $$$$$$\\                                          $$\\                            $$$$$$\\                                    \n");
+    Sleep(500);
+    printf("$$  __$$\\                                         \\__|                          $$  __$$\\                                   \n");
+    Sleep(500);
+    printf("$$ /  \\__|$$\\   $$\\  $$$$$$\\   $$$$$$$\\  $$$$$$$\\ $$\\ $$$$$$$\\   $$$$$$\\        $$ /  \\__| $$$$$$\\  $$$$$$\\$$$$\\   $$$$$$\\  \n");
+    Sleep(500);
+    printf("$$ |$$$$\\ $$ |  $$ |$$  __$$\\ $$  _____|$$  _____|$$ |$$  __$$\\ $$  __$$\\       $$ |$$$$\\  \\____$$\\ $$  _$$  _$$\\ $$  __$$\\ \n");
+    Sleep(500);
+    printf("$$ |\\_$$ |$$ |  $$ |$$$$$$$$ |\\$$$$$$\\  \\$$$$$$\\  $$ |$$ |  $$ |$$ /  $$ |      $$ |\\_$$ | $$$$$$$ |$$ / $$ / $$ |$$$$$$$$ |\n");
+    Sleep(500);
+    printf("$$ |  $$ |$$ |  $$ |$$   ____| \\____$$\\  \\____$$\\ $$ |$$ |  $$ |$$ |  $$ |      $$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|\n");
+    Sleep(500);
+    printf("\\$$$$$$  |\\$$$$$$  |\\$$$$$$$\\ $$$$$$$  |$$$$$$$  |$$ |$$ |  $$ |\\$$$$$$$ |      \\$$$$$$  |\\$$$$$$$ |$$ | $$ | $$ |\\$$$$$$$\\ \n");
+    Sleep(500);
+    printf(" \\______/  \\______/  \\_______|\\_______/ \\_______/ \\__|\\__|  \\__| \\____$$ |       \\______/  \\_______|\\__| \\__| \\__| \\_______|\n");
+    Sleep(500);
+    printf("                                                                $$\\   $$ |                                                  \n");
+    Sleep(500);
+    printf("                                                                \\$$$$$$  |                                                  \n");
+    Sleep(500);
+    printf("                                                                 \\______/                                                   \n");
 }
