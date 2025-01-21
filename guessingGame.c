d2ff7abb286ae5cd3e5002ca25407b7e2760dc4c
keithkattt <k3ith4rwil@gmail.com>
2025-01-21 16:38:11 +0800

    Add ascii art for welcome screen wip

--- guessingGame.c
+++ guessingGame.c
@@ -1,16 +1,19 @@
 // A guessing game program
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 
-//Generates random num from 1 - 100, parameter for how many times you use it
-int randomNum();
-
-//Prompts the user and returns the value 
-int userInput();
+int randomNum();//Generates random num from 1 - 100, parameter for how many times you use it 
+int userInput(); //Prompts the user and returns the value
+int asciiArt(); //Prints ASCII art
 
 int main(){
-    
+
+    printf("Welcome to the guessing game!\n");
+    printf("I have a number between 1 and 100, can you guess it?\n");
+
     int userGuess = userInput();
     int computerNum = randomNum(0);
 
@@ -38,4 +41,17 @@ int userInput(){
     printf("What is your guess?: ");
     scanf("%d", &numInput);
     return numInput;
+}
+
+int asciiArt(){
+    FILE *file = fopen("asciiArt.txt", "w");
+    if (file == NULL) {
+        perror ("Error opening file");
+        return 1;
+    }
+
+
+    const char *art[] = {
+        
+    }
 }
