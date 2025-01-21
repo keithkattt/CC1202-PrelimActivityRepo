3e4d2e98c95cda7098bbf49134296f94e3ef548e
keithkattt <k3ith4rwil@gmail.com>
2025-01-21 19:40:29 +0800

    Added typing style to the text

--- guessingGame.c
+++ guessingGame.c
@@ -8,13 +8,13 @@
 int randomNum();//Generates random num from 1 - 100, parameter for how many times you use it 
 int userInput(); //Prompts the user and returns the value
 void guessingGameHeader(); //Prints ASCII art
+void welcomeMessage(); //Prints welcome message
+void print_in_style(const char *str, int delay); //Prints in style
 
 int main(){
 
     guessingGameHeader();
-    printf("Welcome to the guessing game!\n");
-    printf("I have a number between 1 and 100, can you guess it?\n");
-   
+    welcomeMessage();
 
     int userGuess = userInput();
     int computerNum = randomNum(0);
@@ -40,31 +40,48 @@ int randomNum(){
 
 int userInput(){
     int numInput;
-    printf("What is your guess?: ");
+    const char *userInput = {"What is your guess?: "};
+    print_in_style(userInput, 50);
     scanf("%d", &numInput);
     return numInput;
 }
 
 void guessingGameHeader(){
     printf(" $$$$$$\\                                          $$\\                            $$$$$$\\                                    \n");
-    Sleep(500);
     printf("$$  __$$\\                                         \\__|                          $$  __$$\\                                   \n");
     Sleep(500);
     printf("$$ /  \\__|$$\\   $$\\  $$$$$$\\   $$$$$$$\\  $$$$$$$\\ $$\\ $$$$$$$\\   $$$$$$\\        $$ /  \\__| $$$$$$\\  $$$$$$\\$$$$\\   $$$$$$\\  \n");
-    Sleep(500);
     printf("$$ |$$$$\\ $$ |  $$ |$$  __$$\\ $$  _____|$$  _____|$$ |$$  __$$\\ $$  __$$\\       $$ |$$$$\\  \\____$$\\ $$  _$$  _$$\\ $$  __$$\\ \n");
     Sleep(500);
     printf("$$ |\\_$$ |$$ |  $$ |$$$$$$$$ |\\$$$$$$\\  \\$$$$$$\\  $$ |$$ |  $$ |$$ /  $$ |      $$ |\\_$$ | $$$$$$$ |$$ / $$ / $$ |$$$$$$$$ |\n");
-    Sleep(500);
     printf("$$ |  $$ |$$ |  $$ |$$   ____| \\____$$\\  \\____$$\\ $$ |$$ |  $$ |$$ |  $$ |      $$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|\n");
     Sleep(500);
     printf("\\$$$$$$  |\\$$$$$$  |\\$$$$$$$\\ $$$$$$$  |$$$$$$$  |$$ |$$ |  $$ |\\$$$$$$$ |      \\$$$$$$  |\\$$$$$$$ |$$ | $$ | $$ |\\$$$$$$$\\ \n");
-    Sleep(500);
     printf(" \\______/  \\______/  \\_______|\\_______/ \\_______/ \\__|\\__|  \\__| \\____$$ |       \\______/  \\_______|\\__| \\__| \\__| \\_______|\n");
     Sleep(500);
     printf("                                                                $$\\   $$ |                                                  \n");
-    Sleep(500);
     printf("                                                                \\$$$$$$  |                                                  \n");
-    Sleep(500);
     printf("                                                                 \\______/                                                   \n");
+}
+
+void welcomeMessage(){
+    const char *welcome[] = {
+        "Welcome to the guessing game!",
+        "The computer will generate a random number between 1 and 100.",
+        "You will have to guess the number.",
+        "You have 10 treis to guess the number.",
+    };        
+
+    for(int i = 0; i < 4; i++){
+        print_in_style(welcome[i], 50);
+        printf("\n");
+    }
+
+}
+    
+void print_in_style(const char *str, int delay){
+    for(int i = 0; str[i] != '\0'; i++){
+        printf("%c", str[i]);
+        Sleep(delay);
+    }
 }
