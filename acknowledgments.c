a1c3ffc8f0fcef6f90cecd7bdf5611ecf54f71f3
keithkattt <keith110606@gmail.com>
2025-02-01 21:09:29 +0800

    Finalized Acknowledgement

--- acknowledgments.c
+++ acknowledgments.c
@@ -7,50 +7,83 @@
 void gotoxy(int x, int y);
 void SetColorAndBackground(int ForgC, int BackC);
 void ResetColor();
-void acknowledgementHeader();
-void acknowledgementText();
+void PrintEffect(const char *str, int delay);
+void acknowledgement();
 
 int main(){
-    acknowledgementHeader();
-
+    acknowledgement();
     return 0;
 }
 
-void acknowledgementHeader(){
+void acknowledgement(){
     system("cls");
-    SetColorAndBackground(6, 0);    
-    gotoxy(20, 0); printf("     _        _                        _          _                                 _   "); usleep(100000);  
-    gotoxy(20, 1); printf("    / \\   ___| | ___ __   _____      _| | ___  __| | __ _  ___ _ __ ___   ___ _ __ | |_ "); usleep(100000);
-    gotoxy(20, 2); printf("   / _ \\ / __| |/ / '_ \\ / _ \\ \\ /\\ / / |/ _ \\/ _` |/ _` |/ _ \\ '_ ` _ \\ / _ \\ '_ \\| __|"); usleep(100000);
-    gotoxy(20, 3); printf("  / ___ \\ (__|   <| | | | (_) \\ V  V /| |  __/ (_| | (_| |  __/ | | | | |  __/ | | | |_ "); usleep(100000);
-    gotoxy(20, 4); printf(" /_/   \\\\_\\___|_\\_\\_| |_|\\___/ \\_/\\_/ |_|\\___|\\__,_|\\__, |\\___|_| |_| |_|\\___|_| |_|\\__|"); usleep(100000);
-    gotoxy(20, 5); printf("                                                    |___/                                ");
-    ResetColor();
-}
+    SetColorAndBackground(6, 0); 
+    const char *header[] = {
+        "     _        _                        _          _                                 _   ",
+        "    / \\   ___| | ___ __   _____      _| | ___  __| | __ _  ___ _ __ ___   ___ _ __ | |_ ",
+        "   / _ \\ / __| |/ / '_ \\ / _ \\ \\ /\\ / / |/ _ \\/ _` |/ _` |/ _ \\ '_ ` _ \\ / _ \\ '_ \\| __|",
+        "  / ___ \\ (__|   <| | | | (_) \\ V  V /| |  __/ (_| | (_| |  __/ | | | | |  __/ | | | |_ ",
+        " /_/   \\\\_\\___|_\\_\\_| |_|\\___/ \\_/\\_/ |_|\\___|\\__,_|\\__, |\\___|_| |_| |_|\\___|_| |_|\\__|\n",
+        "                                                    |___/                                "
+    };
 
-void acknowledgementText(){
+    int headerSize = sizeof(header) / sizeof(header[0]);
+    int headerHeight = 0;
+
+    for (int i = 0; i < headerSize; i++) {
+        gotoxy(20, headerHeight++);
+        printf("%s\n", header[i]); // Print the current line
+        usleep(100000);            // 50 milliseconds delay between lines
+        
+    }
+    ResetColor();
+
+    
     const char *acknowledgement[] = {
-        " ______________________________________________________________________________________"  
-        "|  Splash, Menu, Acknowledgement, Debugging:                                           |"
-        "|  Keith Arwil Umali || 0961 279 8645 || keith110606@gmail.com || BSCS-DS-1A           |"
-        "|______________________________________________________________________________________|"
-        "|  Guessing Game by:                                                                   |"
-        "|  Lance Aclan || 0967 276 0722 || lancemercadoaclan@gmail.com || BSCS-DS-1A           |"
-        "|______________________________________________________________________________________|"
-        "|                                                                                      |"
-        "|                                                                                      |"
-        "|                                                                                      |"
-        "|                                                                                      |"
-        "|                                                                                      |"
-        "|                                                                                      |"
-        "|                                                                                      |"
-        "|  Racing Game by:                                                                     |"
-        "|  Hannah Kim Tominez || 0962 793 0143 || tominezhannah800@gmail.com || BSCS-DS-1A     |"
-        "|______________________________________________________________________________________|"
+        " __________________________________________________________________________________________", 
+        "|  Splash, Menu, Acknowledgement, Debugging:                                               |",
+        "|  Keith Arwil Umali || 0961 279 8645 || keith110606@gmail.com || BSCS-DS-1A               |",
+        "|__________________________________________________________________________________________|",
+        "|  Guessing Game by:                                                                       |",
+        "|  Lance Aclan || 0967 276 0722 || lancemercadoaclan@gmail.com || BSCS-DS-1A               |",
+        "|__________________________________________________________________________________________|",
+        "|  Number Tricks by:                                                                       |",
+        "|  Angelie Joy Macabuhay || 0905 316 4853 || angeliejoymacabuhay@gmail.com BSCS-DS-1A      |",
+        "|__________________________________________________________________________________________|",
+        "|  Racing Game by:                                                                         |",
+        "|  Hannah Kim Tominez || 0962 793 0143 || tominezhannah800@gmail.com || BSCS-DS-1A         |",
+        "|  Allyn Castanales Tubice || 0962 793 0143 || tominezhannah800@gmail.com || BSCS-DS-1A    |",                                                                                               
+        "|__________________________________________________________________________________________|",
     };
     
-    int splashSize = sizeof(acknowledgement) / sizeof(acknowledgement[0]);
+    int acknowledgementSize = sizeof(acknowledgement) / sizeof(acknowledgement[0]);
+    int acknowledgementHeight = 6;
 
+      // Print each line with a small delay
+    for (int i = 0; i < acknowledgementSize; i++) {
+        gotoxy(18, acknowledgementHeight++);
+        printf("%s\n", acknowledgement[i]); // Print the current line 
+        usleep(100000);            // 50 milliseconds delay between lines
+    }
+        gotoxy(43, 21); 
+        printf("Thank you for using our program"); usleep(1000000); printf("."); usleep(1000000); printf("."); usleep(1000000); printf(".");  
+        usleep(200000); printf("\n"); printf("\033[F\033[K");
+
+
+    for (int i = 0; i < acknowledgementSize; i++) {
+        printf("\033[F\033[K"); // ANSI escape codes to move cursor up and clear line
+        fflush(stdout);         // Ensure output is updated immediately
+        usleep(100000);          // 50 milliseconds delay between lines
+    }
+
+    for (int i = 0; i <= headerSize; i++) {
+      
+        printf("\033[F\033[K"); // ANSI escape codes to move cursor up and clear line
+        fflush(stdout);         // Ensure output is updated immediately
+        usleep(100000);          // 50 milliseconds delay between lines
+    }
+    
+    
 }
 
 
@@ -73,3 +106,9 @@ void ResetColor() {
     SetColorAndBackground(15, 0); // Reset text color to white and background to black
 }
 
+void PrintEffect(const char *str, int delay) {
+    for(int i = 0; str[i] != '\0'; i++) {
+        printf("%c", str[i]);
+        Sleep(delay);
+    }
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,12 @@
- 
+    for (int i = 0; i < acknowledgementSize; i++) {
+        printf("\033[F\033[K"); // ANSI escape codes to move cursor up and clear line
+        fflush(stdout);         // Ensure output is updated immediately
+        usleep(90000);          // 50 milliseconds delay between lines
+    }
+
+    for (int i = 0; i < headerSize; i++) {
+      
+        printf("\033[F\033[K"); // ANSI escape codes to move cursor up and clear line
+        fflush(stdout);         // Ensure output is updated immediately
+        usleep(90000);          // 50 milliseconds delay between lines
+    }
